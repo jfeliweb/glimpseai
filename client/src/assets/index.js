8871134a9d0066dbc09f0eb1a6943dfcb174394a
Jean Felisme <jeazy@bizdotsmedia.com>
2023-02-08 18:17:05 -0500

    Setup App Header and Routes

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,5 @@
-import { download } from './download.png'
-import { logo } from './logo.svg'
-import { preview } from './preview.png'
+import download from './download.png'
+import logo from './logo.svg'
+import preview from './preview.png'
 
 export { download, logo, preview }
