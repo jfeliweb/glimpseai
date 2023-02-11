a07547160769fa1d69c1aa4608ea14466b91ee00
Jean Felisme <jeazy@bizdotsmedia.com>
2023-02-11 00:22:11 -0500

    setup Routes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 
 import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
+
+app.use('/api/posts', postRoutes)
+app.use('/api/dalle', dalleRoutes)
 
 app.get('/', async (req, res) => {
 	res.send('Hello from your AI server!')
