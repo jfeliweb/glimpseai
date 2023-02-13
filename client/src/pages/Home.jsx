ee48ce32ee9ef09ea9c713af18e2c0082a9279cb
Jean Felisme <jeazy@bizdotsmedia.com>
2023-02-13 00:52:48 -0500

    setup retreaval of all posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,6 +13,31 @@ const Home = () => {
 	const [allPost, setAllPost] = useState(null)
 	const [searchText, setSearchText] = useState('')
 
+	useEffect(() => {
+		const fetchPosts = async () => {
+			setLoading(true)
+
+			try {
+				const response = await fetch('http://localhost:8080/api/v1/posts', {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				})
+				if (response.ok) {
+					const result = await response.json()
+
+					setAllPost(result.data.reverse())
+				}
+			} catch (error) {
+			} finally {
+				setLoading(false)
+			}
+		}
+
+		fetchPosts()
+	}, [])
+
 	return (
 		<section className='max-w-7xl mx-auto'>
 			<div>
@@ -41,7 +66,7 @@ const Home = () => {
 							{searchText ? (
 								<RenderCards data={[]} title='No search results found' />
 							) : (
-								<RenderCards data={[]} title='No posts found' />
+								<RenderCards data={allPost} title='No posts found' />
 							)}
 						</div>
 					</>
