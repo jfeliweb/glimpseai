f55b13cb02dd79544f9bcadb939374e7852dba93
Jean Felisme <jeazy@bizdotsmedia.com>
2023-02-13 00:46:18 -0500

    completed handle submit func send img to homepage

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -39,7 +39,32 @@ const CreatePost = () => {
 		}
 	}
 
-	const handleSubmit = () => {}
+	const handleSubmit = async e => {
+		e.preventDefault()
+
+		if (form.prompt && form.photo) {
+			setLoading(true)
+
+			try {
+				const response = await fetch('http://localhost:8080/api/v1/posts', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(form),
+				})
+
+				await response.json()
+				navigate('/')
+			} catch (error) {
+				alert(error)
+			} finally {
+				setLoading(false)
+			}
+		} else {
+			alert('Please enter a prompt to generate an image to share.')
+		}
+	}
 
 	const handleChange = e => {
 		setForm({ ...form, [e.target.name]: e.target.value })
