57703323b65ca36ad76f833282463a1a0ad224a3
Jean Felisme <jeazy@bizdotsmedia.com>
2023-03-13 14:11:17 -0400

    prod push

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
 		if (form.prompt) {
 			try {
 				setGenerateImg(true)
-				const response = await fetch('http://localhost:8080/api/v1/dalle', {
+				const response = await fetch('https://glimpseai-server.onrender.com/api/v1/dalle', {
 					method: 'POST',
 					headers: {
 						'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ const CreatePost = () => {
 			setLoading(true)
 
 			try {
-				const response = await fetch('http://localhost:8080/api/v1/posts', {
+				const response = await fetch('https://glimpseai-server.onrender.com/api/v1/posts', {
 					method: 'POST',
 					headers: {
 						'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
 			setLoading(true)
 
 			try {
-				const response = await fetch('http://localhost:8080/api/v1/posts', {
+				const response = await fetch('https://glimpseai-server.onrender.com/api/v1/posts', {
 					method: 'GET',
 					headers: {
 						'Content-Type': 'application/json',
